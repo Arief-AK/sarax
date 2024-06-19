ed97b8f33ea6981679f47ec6ca37ade84711b1ef
Arief <mariefak22@gmail.com>
2024-06-19 14:09:46 +0200

    added option to perform clean build if build failed in container menu

--- scripts/container/container_menu.py
+++ scripts/container/container_menu.py
@@ -8,15 +8,18 @@ def menu():
         print("What would you like to run?")
         print("1. Simulator")
         print("2. Sarax Framework")
-        print("3. Exit")
+        print("3. Clean Build (if build failing)")
+        print("4. Exit")
 
-        choice = int(input("\nChoice (1 - 3): "))
+        choice = int(input("\nChoice (1 - 4): "))
 
         if choice == 1:
             os.system("cd $SARAX_WS/PX4-Autopilot && ./sarax_plus_sitl.bash")
         elif choice == 2:
             os.system("roslaunch m4e_mani_base sarax_plus_sitl.launch")
         elif choice == 3:
+            os.system("cd ~/dependencies && ./ubuntu.sh")
+        elif choice == 4:
             done = True
         else:
             print("Invalid input")
